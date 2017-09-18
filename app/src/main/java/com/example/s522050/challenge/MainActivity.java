0adbcf89de1b4c7081788f522670ffce9de799dc
Gunhee Lee <s522050@nwmissouri.edu>
2017-09-18 13:38:08 -0500

    .

--- app/src/main/java/com/example/s522050/challenge/MainActivity.java
+++ app/src/main/java/com/example/s522050/challenge/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private String ORDER_URL = "https://shopicruit.myshopify.com/admin/orders.json?page=1&access_token=c32313df0d0ef512ca64d5b336a0d7c6";
     public boolean networkOk;
 
+    //Butter Knife
     @BindView(R.id.output_total_spent)TextView total_spent_result;
     @BindView(R.id.output_total_quantity)TextView total_quantity_result;
 
